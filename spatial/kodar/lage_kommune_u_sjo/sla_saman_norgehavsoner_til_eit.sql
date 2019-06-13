0586300dea5a0b3a5a755c3702ac830997ef4b2e
kiosvoll-HFD <knut.ivar.osvoll@helse-forde.no>
2019-06-13 23:17:42 +0200

    s

--- spatial/kodar/lage_kommune_u_sjo/sla_saman_norgehavsoner_til_eit.sql
+++ spatial/kodar/lage_kommune_u_sjo/sla_saman_norgehavsoner_til_eit.sql
@@ -1,2 +1,10 @@
 
 SELECT Geography::UnionAggregate(ogr_geometry2) HavGeo into norgehavInvertUnion  FROM norgehavInvert
+
+
+--- dette er prinsippet må skrive det om til lokal kilde file med gjerne grunnkretsar som skal skjerast til.
+select ogr_geometry.STDifference( (select HavGeo from norgehavInvertUnion) ) as NyGeo,kommunenummer,substring(navn,4,100) as navn 
+into #komKart 
+From kommunarnorge
+-- 
+select * From #komkart where kommunenummer < 1700
